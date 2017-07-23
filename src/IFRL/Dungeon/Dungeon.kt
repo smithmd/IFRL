9d09518115082ce4775444da329c6544f681b730
Michael Smith <mike@noiseparticles.com>
2017-07-23 12:27:43 -0400

    converted array of floors to list

--- src/IFRL/Dungeon/Dungeon.kt
+++ src/IFRL/Dungeon/Dungeon.kt
@@ -2,12 +2,12 @@ package IFRL.Dungeon
 
 object Dungeon {
     val floorCount: Int = (Math.random() * 5).toInt() + 1
-    val floors: Array<Floor> = Array(floorCount, { Floor() })
+    val floors: List<Floor> = List(floorCount) { Floor() }
 
     init {
         println("Creating a dungeon with $floorCount floor${if (floorCount > 1) "s" else ""}.")
-        for (i in 0..(floorCount - 1)) {
-            floors[i].drawFloor()
+        for (floor in floors) {
+            floor.drawFloor()
             println()
         }
     }
