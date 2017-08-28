0c6ffd289bf5a9c6df39c601007b253894c075ad
Michael Smith <mike@noiseparticles.com>
2017-08-28 09:49:55 -0400

    cleaned up main

--- src/IFRL/main.kt
+++ src/IFRL/main.kt
@@ -1,13 +1,11 @@
 package IFRL
 
-import IFRL.Actor.Actor
 import IFRL.Actor.Player
 import IFRL.Dungeon.Dungeon
 
 fun main(args: Array<String>) {
     val dungeon = Dungeon
-//    val actors: MutableList<Actor> = MutableList(1)
-
     val player = Player()
-//    actors.add(player)
+
+    // game loop will go here
 }
